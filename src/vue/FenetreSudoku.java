c026da96b3c20ffcfa3f444bba5d6cdeafb57766
florent <delmotte.florent@numericable.fr>
2013-11-04 23:35:41 +0100

    suppression case interne «Casse» + simplification TextListener

--- src/vue/FenetreSudoku.java
+++ src/vue/FenetreSudoku.java
@@ -2,7 +2,6 @@ package vue;
 
 import javax.swing.*;
 
-import vue.Casse;
 import vue.FenetreSudo;
 
 import model.GrilleGamer;
@@ -22,23 +21,23 @@ import java.util.List;
 
 class FenetreSudo extends JFrame implements Serializable{	
 	private JButton b_corrige,b_ok, sauvegarde;
-	public GrilleGamer s1;
-	private List<JPanel> pC;//sert à rien pour l'instant
+	public GrilleGamer game;
+	private List<JPanel> listCarre;//sert à rien pour l'instant
 
 	public FenetreSudo(){
-		s1=new GrilleGamer();
+		game=new GrilleGamer();
 		initAttr();
 		initFenetre();
 	}
 
 	public FenetreSudo(GrilleGamer s){
-		s1=s;
+		game=s;
 		initAttr();
 		initFenetre();
 	}
 
 	public void initAttr(){
-		this.pC = new ArrayList<JPanel>();
+		this.listCarre = new ArrayList<JPanel>();
 		this.b_corrige=new JButton("corriger");
 		this.b_ok=new JButton("OK");
 		this.sauvegarde= new JButton("sauvegarde");
@@ -48,7 +47,7 @@ class FenetreSudo extends JFrame implements Serializable{
 		setTitle("Sudoku");
 		Container container=this.getContentPane();
 
-		//Creation d'un "tableau" de 3*3 cases pour mettre les 9 cases dedans
+		//Creation d'un "tableau" de 3*3 cases pour mettre les cases dedans
 		GridLayout layoutGrille=new GridLayout(3,3);
 		JPanel grillePanel=new JPanel();
 		grillePanel.setLayout(layoutGrille);
@@ -56,8 +55,8 @@ class FenetreSudo extends JFrame implements Serializable{
 		//generation des grandes cases une par une
 		for (int cmp=0; cmp<9; cmp++){
 			JPanel carre =carrePanel(cmp);
-			pC.add(carre);
-			grillePanel.add(pC.get(cmp));
+			listCarre.add(carre);
+			grillePanel.add(listCarre.get(cmp));
 		}
 
 		//association des boutons aux evenements
@@ -77,20 +76,18 @@ class FenetreSudo extends JFrame implements Serializable{
 	public void setBorderCarre(JPanel g){
 		g.setBorder(BorderFactory.createCompoundBorder(
 				BorderFactory.createEmptyBorder(0, 0, 0, 0),
-				BorderFactory.createLineBorder(Color.BLACK, 1)	)
+				BorderFactory.createLineBorder(Color.BLACK, 1))
 		);
 	}
 
-	/** Renvois un JPanel avec un GridLayout 3*3
-	 **/
-
+	/***/
 	public JPanel carrePanel(int cmp){
 		JPanel carre=new JPanel();
 		carre.setLayout(new GridLayout(3,3));
 		//generation des cases contenu dans le panelCarre
 		for (int i=0; i<3; i++) {
 			for (int j=0; j<3; j++){
-				String val = s1.getValeur(i+(3*(cmp/3)),j+(3*(cmp%3)));
+				String val = game.getValeur(i+(3*(cmp/3)),j+(3*(cmp%3)));
 
 				if(val.compareTo("0") > 0){	//si valeur > 0 la case est découverte
 					carre.add(new JLabel(val));
@@ -99,7 +96,8 @@ class FenetreSudo extends JFrame implements Serializable{
 					if (cmp %2 != 0)
 						carre.setBackground(Color.GRAY);
 				}else{
-					JTextField zoneDeTexte= new Casse(i,j,s1).retour();//simplifier en ne donnant pas s1????
+					JTextField zoneDeTexte= new JTextField(1);
+					zoneDeTexte.addActionListener(new TextListener(zoneDeTexte, i, j));
 					carre.add(zoneDeTexte);
 				}
 			}
@@ -108,9 +106,8 @@ class FenetreSudo extends JFrame implements Serializable{
 		return carre;
 	}
 
-//------------------------------------------------------------
-//------------------------- Listener -------------------------
-//------------------------------------------------------------
+	//------------------------- Listeners -------------------------
+
 	class OkListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e){
@@ -122,7 +119,7 @@ class FenetreSudo extends JFrame implements Serializable{
 	class corrigeListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e){
-			if(s1.verifGrille())
+			if(game.verifGrille())
 				System.out.println("grille true ");
 			else
 				System.out.println("grille false ");
@@ -136,7 +133,7 @@ class FenetreSudo extends JFrame implements Serializable{
 			try {
 				java.io.FileOutputStream  f = new FileOutputStream("Sudoku.sdku");
 				java.io.ObjectOutputStream s = new ObjectOutputStream(f);
-				s.writeObject(s1);
+				s.writeObject(game);
 				s.flush();
 				s.close();
 				f.close();
@@ -147,40 +144,24 @@ class FenetreSudo extends JFrame implements Serializable{
 			}
 		}
 	}
-}
 
-//
-// casse et btext vont ensemble
-// possibilité de suprimmer case pour le remplacer par une simple methode
-class Casse{
-	private GrilleGamer s;
-	private int i,j;
-	private JTextField text;
+	class TextListener implements ActionListener {
+		private JTextField text;
+		private int i,j;
 
-	public Casse(int ii,int jj,GrilleGamer ss){
-		i=ii;j=jj;s=ss;
-		text=new JTextField(1);
-		TextListener b = new TextListener(text,ss,i,j);
-		text.addActionListener(b);
+		public TextListener(JTextField text, int coordX,int coordY){
+			this.i=coordX;
+			this.j=coordY;
+			this.text=text;
+		}
 
+		@Override
+		public void actionPerformed(ActionEvent e){
+			//faire en sorte que l'on ne peux pas entrer autre que 1 2 3 4 5  6 7 8 9 
+			int res= Integer.parseInt(text.getText());
+			game.setGrille(i,j,res);
+			System.out.println(""+game);
+		}
 	}
-	/*retourne une zone de texte qui est lier a une case i,j
-	 de l'objet ss de type sudokuARemplir  */
-	public JTextField retour(){
-		return text;
-	}
-}
-class TextListener implements ActionListener {
-	private JTextField t;
-	private GrilleGamer s;
-	private int i,j;
-	public TextListener(JTextField tt,GrilleGamer ss,int ii,int jj){i=ii;j=jj;t=tt;s=ss;}
 
-	public void actionPerformed(ActionEvent e){
-		//faire en sorte que l'on ne peux pas entrer autre que 1 2 3 4 5  6 7 8 9 
-		int res= Integer.parseInt(t.getText());
-		s.setGrille(i,j,res);
-		System.out.println(""+s);
-		//	return t;
-	}
 }
